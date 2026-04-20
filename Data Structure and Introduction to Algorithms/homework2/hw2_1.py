97650140c5dda42c51653492c4de34abdac0b690
night <hjq122418@163.com>
2026-04-20 22:02:05 +0800

    提交已完成的作业

--- Data Structure and Introduction to Algorithms/homework2/hw2_1.py
+++ Data Structure and Introduction to Algorithms/homework2/hw2_1.py
@@ -2,3 +2,44 @@
 请根据课件上给出的双端队列的ADT，给出一个完整的基于数组的双端队列的 python
 实现。
 '''
+class deque:
+    def __init__(self):
+        self.data = []
+
+    def add_first(self, e):
+        self.data.insert(0, e)
+
+    def add_last(self, e):
+        self.data.append(e)
+
+    def delete_first(self):
+        if self.is_empty():
+            raise Exception("双端队列为空")
+        return self._data.pop(0)
+    
+    def delete_last(self):
+        if self.is_empty():
+            raise Exception("双端队列为空")
+        return self._data.pop()
+    
+    def first(self):
+        if self.is_empty():
+            raise Exception("双端队列为空")
+        return self._data[0]
+    
+    def last(self):
+        if self.is_empty():
+            raise Exception("双端队列为空")
+        return self._data[-1]
+    
+    def __len__(self):
+        return len(self.data)
+    
+    def init(self, D):
+        self.data = D.data
+    
+    def is_empty(self):
+        return len(self) == 0
+
+    def clear(self):
+        self._data = []    
---
+++ Data Structure and Introduction to Algorithms/homework2/hw2_5.py
@@ -0,0 +1,5 @@
+'''
+如果充分利用哨兵节点（_sentinel），我们可以简化链式二叉树 LinkedBinaryTree
+的实现过程。这里，哨兵节点为根节点的父节点，且根节点为哨兵节点的左孩子节点。
+请写出新的LinkedBinaryTree的_delete 方法。
+'''
