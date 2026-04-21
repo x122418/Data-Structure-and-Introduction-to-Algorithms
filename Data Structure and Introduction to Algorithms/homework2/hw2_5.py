d7fca82051db70a3947891257e446524fde4f9dc
night <hjq122418@163.com>
2026-04-21 15:00:30 +0800

    更新代码+添加pdf文件

--- Data Structure and Introduction to Algorithms/homework2/hw2_5.py
+++ Data Structure and Introduction to Algorithms/homework2/hw2_5.py
@@ -3,3 +3,28 @@
 的实现过程。这里，哨兵节点为根节点的父节点，且根节点为哨兵节点的左孩子节点。
 请写出新的LinkedBinaryTree的_delete 方法。
 '''
+
+def _delete(self, p):
+    """Delete the node p, and replace it with its child, if any.
+    Return the element that had been stored at p.
+    Raise ValueError if p is invalid or p has two children.
+    """
+    if p is self._sentinel or p is None:
+        raise ValueError('Invalid node')
+
+    if self.num_cildren(p) == 2:
+        raise ValueError('Node has two children')
+    
+    child = p._left if p._left else p._right # might be None
+
+    if child is not None:
+        child._parent = p._parent
+    
+    if p is p._parent.left:
+        p._parent._left = child
+    else:
+        p._parent._right = child
+    
+    self._size -= 1
+    p._parent = p                    # convention for deprecated node
+    return p._element
---
+++ Data Structure and Introduction to Algorithms/homework2/hw2_7.py
@@ -0,0 +1,10 @@
+'''
+给出类HeapPriorityQueue中_upheap和_downheap方法的非递归实现。
+'''
+
+def _upheap(self, j):
+
+    return
+
+def _downheap(self, j):
+    n = len(self._data)
