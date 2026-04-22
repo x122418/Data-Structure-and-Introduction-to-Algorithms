d8194dcf72c4e9ae8cd0fe5d4fceb88311f8747d
x122418 <hjq122418@gmail.com>
2026-04-22 19:00:45 +0800

    update hw2

--- homework2/hw2_7.py
+++ homework2/hw2_7.py
@@ -1,10 +1,34 @@
 '''
 给出类HeapPriorityQueue中_upheap和_downheap方法的非递归实现。
 '''
+class HeapPriorityQueue:
+    def __init__(self):
+        self._data:list[int] = []
+    
+    def swap(self,i, j):
+        self._data[i], self._data[j] = self._data[j], self._data[i]
 
-def _upheap(self, j):
+    def _upheap(self, j):
+        while j > 0:
+            parent = (j-1) // 2
+            if self._data[j] < self._data[parent]:
+                self.swap(j, parent)
+            else:
+                break
+            j = parent
 
-    return
+    def _downheap(self, j):
+        n = len(self._data)
+        while True:
+            left = 2 * j + 1
+            right = 2 * j + 2
+            smallest = j
 
-def _downheap(self, j):
-    n = len(self._data)
+            if left < n and self._data[left] < self._data[smallest]:
+                smallest = left
+            if right < n and self._data[right] < self._data[smallest]:
+                smallest = right
+            if smallest == j:
+                break
+            self.swap(j, smallest)
+            j = smallest
